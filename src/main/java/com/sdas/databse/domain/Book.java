10f43df7ff5441571eda7147155458410602fc52
Shreyojit <shreyojitdas95@gmail.com>
2024-05-30 02:54:07 +0530

    Create the readOne endpoint and done iT

--- src/main/java/com/sdas/databse/domain/Book.java
+++ src/main/java/com/sdas/databse/domain/Book.java
@@ -1,18 +1,102 @@
 package com.sdas.databse.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@Entity
+@Table(name = "books")
 public class Book {
 
+    @Id
     private String isbn;
     private String title;
-    private Long authorId;
 
+    public Book(String isbn, String title) {
+        this.isbn = isbn;
+        this.title = title;
+    }
+
+    public Book() {
+    }
+
+    public static BookBuilder builder() {
+        return new BookBuilder();
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Book)) return false;
+        final Book other = (Book) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$isbn = this.getIsbn();
+        final Object other$isbn = other.getIsbn();
+        if (this$isbn == null ? other$isbn != null : !this$isbn.equals(other$isbn)) return false;
+        final Object this$title = this.getTitle();
+        final Object other$title = other.getTitle();
+        if (this$title == null ? other$title != null : !this$title.equals(other$title)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Book;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $isbn = this.getIsbn();
+        result = result * PRIME + ($isbn == null ? 43 : $isbn.hashCode());
+        final Object $title = this.getTitle();
+        result = result * PRIME + ($title == null ? 43 : $title.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Book(isbn=" + this.getIsbn() + ", title=" + this.getTitle() + ")";
+    }
+
+    public static class BookBuilder {
+        private String isbn;
+        private String title;
+
+        BookBuilder() {
+        }
+
+        public BookBuilder isbn(String isbn) {
+            this.isbn = isbn;
+            return this;
+        }
+
+        public BookBuilder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Book build() {
+            return new Book(this.isbn, this.title);
+        }
+
+        public String toString() {
+            return "Book.BookBuilder(isbn=" + this.isbn + ", title=" + this.title + ")";
+        }
+    }
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "author_id")
+    private Author author;
 }
