f94722945d17def3575c2b54dc19f9b63f3f28c5
Shreyojit <shreyojitdas95@gmail.com>
2024-05-30 02:19:19 +0530

    Connected to Postgres

---
+++ src/main/java/com/sdas/databse/repositories/AuthorRepository.java
@@ -0,0 +1,4 @@
+package com.sdas.databse.repositories;
+
+public interface AuthorRepository {
+}
--- src/test/java/com/sdas/databse/repositories/BookDaoImplIntegrationTests.java
+++ src/test/java/com/sdas/databse/repositories/BookDaoImplIntegrationTests.java
@@ -1,4 +1,4 @@
-package com.sdas.databse.dao.impl;
+package com.sdas.databse.repositories;
 
 import com.sdas.databse.TestDataUtil;
 import com.sdas.databse.dao.AuthorDao;
@@ -8,14 +8,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class BookDaoImplIntegrationTests {
 
     private AuthorDao authorDao;
@@ -38,6 +41,28 @@ public class BookDaoImplIntegrationTests {
         assertThat(result.get()).isEqualTo(book);
     }
 
+    @Test
+    public void testThatMultipleBooksCanBeCreatedAndRecalled() {
+        Author author = TestDataUtil.createTestAuthorA();
+        authorDao.create(author);
+
+        Book bookA = TestDataUtil.createTestBookA();
+        bookA.setAuthorId(author.getId());
+        underTest.create(bookA);
+
+        Book bookB = TestDataUtil.createTestBookB();
+        bookB.setAuthorId(author.getId());
+        underTest.create(bookB);
+
+        Book bookC = TestDataUtil.createTestBookC();
+        bookC.setAuthorId(author.getId());
+        underTest.create(bookC);
+
+        List<Book> result = underTest.find();
+        assertThat(result)
+                .hasSize(3)
+                .containsExactly(bookA, bookB, bookC);
+    }
 
 
 }
