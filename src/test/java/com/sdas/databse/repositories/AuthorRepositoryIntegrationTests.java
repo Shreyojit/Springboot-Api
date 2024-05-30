4b44a781f53cfed8bee47e6287fc783831530a09
Shreyojit <shreyojitdas95@gmail.com>
2024-05-30 10:04:55 +0530

    All Integration HQl

--- src/test/java/com/sdas/databse/repositories/AuthorRepositoryIntegrationTests.java
+++ src/test/java/com/sdas/databse/repositories/AuthorRepositoryIntegrationTests.java
@@ -73,4 +73,35 @@ public class AuthorRepositoryIntegrationTests {
         assertThat(result).isEmpty();
     }
 
+    @Test
+    public void testThatGetAuthorWithAgeLessThan(){
+        Author testAuthorA = TestDataUtil.createTestAuthorA();
+        underTest.save(testAuthorA);
+        Author testAuthorB = TestDataUtil.createTestAuthorB();
+        underTest.save(testAuthorB);
+        Author testAuthorC = TestDataUtil.createTestAuthorC();
+        underTest.save(testAuthorC);
+
+        Iterable<Author> result = underTest.ageLessThan(50);
+        assertThat(result).containsExactly(testAuthorB,testAuthorC);
+
+
+    }
+
+
+    @Test
+    public void testThatGetAuthorWithAgeGreaterThan(){
+        Author testAuthorA = TestDataUtil.createTestAuthorA();
+        underTest.save(testAuthorA);
+        Author testAuthorB = TestDataUtil.createTestAuthorB();
+        underTest.save(testAuthorB);
+        Author testAuthorC = TestDataUtil.createTestAuthorC();
+        underTest.save(testAuthorC);
+
+        Iterable<Author> result = underTest.ageMoreThan(50);
+        assertThat(result).containsExactly(testAuthorA);
+    }
+
+
+
 }
